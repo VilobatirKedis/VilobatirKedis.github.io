3d6b6547c4e52658c1e7bb2ca4f315d42b61ebe8
VilobatirKedis <vilobatirk@gmail.com>
2021-04-01 13:26:58 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   setTimeout(function() {
     $(".loader").animate({
       opacity: 0
     }, 300, function() {
       $(".loader").remove();
       $("#handler").addClass("selection");
+      $("#handler2").addClass("textslide");
     });
 
   }, 1800);
-})
+})*/
